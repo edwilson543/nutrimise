e0a4b0788cfb163864c0ae3f05b9b580f8d44d9e
edwilson <edward.e.j.wilson@gmail.com>
2023-07-14 08:40:16 +0100

    Remove incorrect field from serializer

--- reciply/interfaces/rest_api/recipes/serializers.py
+++ reciply/interfaces/rest_api/recipes/serializers.py
@@ -4,7 +4,6 @@ from rest_framework import serializers
 
 class Recipe(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    author = serializers.ImageField(read_only=True)
     name = serializers.CharField(max_length=128)
     description = serializers.CharField()
     created_at = serializers.DateTimeField(read_only=True)
