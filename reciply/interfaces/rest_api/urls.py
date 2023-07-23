ac8c827c1b533d10dd61eff4680265f8d37abe9c
edwilson <edward.e.j.wilson@gmail.com>
2023-07-23 15:22:56 +0100

    Add registration view

--- reciply/interfaces/rest_api/urls.py
+++ reciply/interfaces/rest_api/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     urls.path("login/", user_views.Login.as_view(), name="login"),
     urls.path("logout/", knox_views.LogoutView.as_view(), name="logout"),
     urls.path("logout-all/", knox_views.LogoutAllView.as_view(), name="logout-all"),
+    urls.path("register/", user_views.Register.as_view(), name="register"),
     # ----------
     # Recipes
     # ----------
