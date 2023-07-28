60fd1cb22c04d8887704c6423da7c9a4dac4740c
edwilson <edward.e.j.wilson@gmail.com>
2023-07-28 17:39:12 +0100

    Add menu models to the django admin site

--- reciply/interfaces/admin/admin.py
+++ reciply/interfaces/admin/admin.py
@@ -2,28 +2,46 @@
 from django.contrib import admin
 
 # Local application imports
+from data.menus import models as menu_models
 from data.recipes import models as recipe_models
 
 admin.site.site_header = "Reciply admin"
 
+# ----------
+# Recipes
+# ----------
+
 
 @admin.register(recipe_models.Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = [
-        "id",
-        "author",
-        "name",
-    ]
-    ordering = ["-name"]
-    list_filter = ["author"]
+    list_display = ["id", "author", "name"]
+    ordering = ["name"]
+    search_fields = ["name", "author"]
 
 
 @admin.register(recipe_models.RecipeImage)
 class RecipeImageAdmin(admin.ModelAdmin):
-    list_display = [
-        "id",
-        "recipe",
-        "image",
-    ]
-    ordering = ["recipe"]
-    list_filter = ["recipe", "recipe__author"]
+    list_display = ["id", "recipe", "image"]
+    ordering = ["recipe__name"]
+
+
+# ----------
+# Menus
+# ----------
+
+
+@admin.register(menu_models.Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ["id", "author", "name", "number_of_items"]
+    ordering = ["name"]
+
+    @admin.display()
+    def number_of_items(self, menu: menu_models.Menu) -> int:
+        return menu.items.count()
+
+
+@admin.register(menu_models.MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "menu", "recipe", "day", "meal_time"]
+    ordering = ["menu", "day", "meal_time"]
+    search_fields = ["menu"]
