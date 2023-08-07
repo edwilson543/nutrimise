a715c539182917f41ba20de73d1da86206064d97
edwilson <edward.e.j.wilson@gmail.com>
2023-08-07 19:31:43 +0100

    Add function to create a menu with suggestions

--- reciply/app/menus/_create_menu.py
+++ reciply/app/menus/_create_menu.py
@@ -5,6 +5,8 @@ from django.contrib.auth import models as auth_models
 # Local application imports
 from data.menus import models as menu_models
 
+from . import _add_suggestions_to_menu
+
 
 class MenuNameNotUniqueForAuthor(django_db.IntegrityError):
     pass
@@ -15,7 +17,11 @@ def create_menu(
     author: auth_models.User,
     name: str,
     description: str,
+    add_suggestions: bool = False,
 ) -> menu_models.Menu:
     if menu_models.Menu.objects.filter(name__iexact=name):
         raise MenuNameNotUniqueForAuthor
-    return menu_models.Menu.new(author=author, name=name, description=description)
+    menu = menu_models.Menu.new(author=author, name=name, description=description)
+    if add_suggestions:
+        _add_suggestions_to_menu.add_suggestions_to_menu(menu=menu)
+    return menu
---
+++ tests/integration/app/menus/test_add_suggestions_to_menu.py
@@ -0,0 +1,33 @@
+# Local application imports
+from app.menus import _add_suggestions_to_menu
+from data import constants
+from tests import factories
+
+
+class TestAddSuggestionsToMenu:
+    def test_adds_suggestion_for_each_day_of_week(self):
+        user = factories.User()
+        menu = factories.Menu(author=user)
+
+        for _ in constants.Day.values:
+            factories.Recipe(author=user)
+
+        menu = _add_suggestions_to_menu.add_suggestions_to_menu(menu=menu)
+
+        for day in constants.Day.values:
+            assert menu.items.get(day=day)
+
+    def test_creates_menu_with_suggestions_too_few_suggestions_to_fill_a_week(self):
+        user = factories.User()
+        menu = factories.Menu(author=user)
+
+        available_recipes = 3
+        for _ in constants.Day.values[:available_recipes]:
+            factories.Recipe(author=user)
+
+        menu = _add_suggestions_to_menu.add_suggestions_to_menu(menu=menu)
+
+        for day in constants.Day.values[:available_recipes]:
+            assert menu.items.get(day=day)
+        for day in constants.Day.values[available_recipes:]:
+            assert not menu.items.filter(day=day).exists()
