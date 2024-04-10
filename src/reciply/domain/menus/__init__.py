3b26e4e06831bd0fb9203fa1fc860175fefe7db2
edwilson543 <edward.e.j.wilson@gmail.com>
2024-04-10 21:42:13 +0100

    Add operation to update menu item

--- src/reciply/domain/menus/__init__.py
+++ src/reciply/domain/menus/__init__.py
@@ -1,3 +1,4 @@
 from ._model import Menu, MenuItem
+from ._operations import update_menu_item_recipe
 from ._optimisation.optimiser import optimise_recipes_for_menu
-from ._queries import get_menus_authored_by_user, MenuDoesNotExist, get_menu
+from ._queries import MenuDoesNotExist, get_menu, get_menus_authored_by_user
