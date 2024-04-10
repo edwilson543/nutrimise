fab36236d66622569135e9b806bc77918477258d
edwilson543 <edward.e.j.wilson@gmail.com>
2024-04-10 17:53:47 +0100

    Add `from_orm_model` method to the `Recipe` model

--- src/reciply/domain/recipes/_model.py
+++ src/reciply/domain/recipes/_model.py
@@ -1,13 +1,27 @@
+from __future__ import annotations
+
 # Standard library imports
 import dataclasses
 
 # Local application imports
 from reciply.data import constants
+from reciply.data.recipes import models as recipe_models
 from reciply.domain import ingredients
 
 
 @dataclasses.dataclass(frozen=True)
 class Recipe:
     id: int
-    meal_times: list[constants.MealTime]
-    nutritional_information: list[ingredients.NutritionalInformation]
+    meal_times: tuple[constants.MealTime]
+    nutritional_information: tuple[ingredients.NutritionalInformation]
+
+    @classmethod
+    def from_orm_model(cls, *, recipe: recipe_models.Recipe) -> Recipe:
+        nutritional_information = ingredients.get_nutritional_information_for_recipe(
+            recipe=recipe, per_serving=True
+        )
+        return cls(
+            id=recipe.id,
+            meal_times=tuple(constants.MealTime(meal_time) for meal_time in recipe.meal_times),
+            nutritional_information=tuple(nutritional_information),
+        )
