69114c6d95997b4a8dfdc552f9cf67df4df40d51
edwilson543 <edward.e.j.wilson@gmail.com>
2024-04-16 21:23:07 +0100

    Only give optimiser recipes meeting dietary requirements

--- tests/integration/app/menus/test_optimise_menu.py
+++ tests/integration/app/menus/test_optimise_menu.py
@@ -16,6 +16,23 @@ def test_optimises_menu():
     assert menu_item.recipe_id == recipe.id
 
 
+def test_optimises_menu_by_excluding_recipes_not_meeting_dietary_requirements():
+    menu = data_factories.Menu()
+    data_factories.MenuRequirements(menu=menu)
+    menu_item = data_factories.MenuItem(menu=menu, recipe_id=None)
+
+    veggie = data_factories.DietaryRequirement()
+    recipe = data_factories.Recipe.create_to_satisfy_dietary_requirements(
+        dietary_requirements=(veggie,), meal_times=[menu_item.meal_time]
+    )
+    data_factories.Recipe(meal_times=[menu_item.meal_time])  # Some non-veggie recipe.
+
+    menus.optimise_menu(menu_id=menu.id)
+
+    menu_item.refresh_from_db()
+    assert menu_item.recipe_id == recipe.id
+
+
 def test_raises_for_menu_without_requirements():
     menu = data_factories.Menu()
 
