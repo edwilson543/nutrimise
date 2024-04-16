ea9b298e21e12bea3d77efd1f17ce665148ecab8
edwilson543 <edward.e.j.wilson@gmail.com>
2024-04-16 20:15:46 +0100

    Add factory for dietary requirement model

--- tests/factories/data/__init__.py
+++ tests/factories/data/__init__.py
@@ -1,4 +1,9 @@
 from ._auth import User
-from ._ingredients import Ingredient, IngredientNutritionalInformation, Nutrient
+from ._ingredients import (
+    DietaryRequirement,
+    Ingredient,
+    IngredientNutritionalInformation,
+    Nutrient,
+)
 from ._menus import Menu, MenuItem, MenuRequirements, NutrientRequirement
 from ._recipes import Recipe, RecipeIngredient
