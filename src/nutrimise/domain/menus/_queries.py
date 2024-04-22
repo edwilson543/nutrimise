ac7c39af011a124a1f0a9022767ca3776804edfe
edwilson543 <edward.e.j.wilson@gmail.com>
2024-04-22 20:42:51 +0100

    Prefetch requirements when getting menu

--- src/nutrimise/domain/menus/_queries.py
+++ src/nutrimise/domain/menus/_queries.py
@@ -12,7 +12,12 @@ class MenuDoesNotExist(Exception):
 
 def get_menu(*, menu_id: int) -> _model.Menu:
     try:
-        menu = menu_models.Menu.objects.prefetch_related("items").get(id=menu_id)
+        menu = menu_models.Menu.objects.prefetch_related(
+            "items",
+            "requirements",
+            "requirements__nutrient_requirements",
+            "requirements__variety_requirements",
+        ).get(id=menu_id)
     except menu_models.Menu.DoesNotExist as exc:
         raise MenuDoesNotExist(menu_id=menu_id) from exc
     return _model.Menu.from_orm_model(menu=menu)
