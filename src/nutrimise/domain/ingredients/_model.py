7269251b1ffb8f57f03ba7dba4d016ffef9f6199
edwilson <edward.e.j.wilson@gmail.com>
2024-05-25 15:05:22 +0100

    Add method to aggregate lists of nutritional information

--- src/nutrimise/domain/ingredients/_model.py
+++ src/nutrimise/domain/ingredients/_model.py
@@ -36,3 +36,48 @@ class NutritionalInformation:
     nutrient: Nutrient
     nutrient_quantity: float
     units: constants.NutrientUnit
+
+    def __add__(self, other: NutritionalInformation) -> NutritionalInformation:
+        if self.nutrient == other.nutrient and self.units == other.units:
+            nutrient_quantity = self.nutrient_quantity + other.nutrient_quantity
+            return NutritionalInformation(
+                nutrient=self.nutrient,
+                nutrient_quantity=nutrient_quantity,
+                units=self.units,
+            )
+        else:
+            raise ValueError(
+                "Cannot add nutritional information for different nutrients."
+            )
+
+    @classmethod
+    def zero(
+        cls,
+        *,
+        nutrient: Nutrient,
+        units: constants.NutrientUnit = constants.NutrientUnit.GRAMS,
+    ) -> NutritionalInformation:
+        return cls(
+            nutrient=nutrient,
+            units=units,
+            nutrient_quantity=0.0,
+        )
+
+    @classmethod
+    def sum_by_nutrient(
+        cls,
+        *,
+        nutritional_information: list[NutritionalInformation],
+        nutrients: list[Nutrient],
+    ) -> list[NutritionalInformation]:
+        """
+        Aggregate a list of nutritional information into a list where each nutrient is unique.
+        """
+        aggregated_nutritional_information = {
+            nutrient: cls.zero(nutrient=nutrient) for nutrient in nutrients
+        }
+        for information in nutritional_information:
+            if information.nutrient in nutrients:
+                aggregated_nutritional_information[information.nutrient] += information
+
+        return list(aggregated_nutritional_information.values())
---
+++ tests/integration/domain/ingredients/test_model.py
@@ -0,0 +1,54 @@
+from nutrimise.data import constants
+from nutrimise.domain import ingredients
+
+from tests.factories import domain as domain_factories
+
+
+class TestNutritionalInformationSumByNutrient:
+    def test_aggregates_list_containing_nutritional_information_for_same_ingredient(
+        self,
+    ):
+        nutrient = domain_factories.Nutrient()
+        other_nutrient = domain_factories.Nutrient()
+        units = constants.NutrientUnit.GRAMS
+
+        unaggregated_list = [
+            domain_factories.NutritionalInformation(
+                nutrient=nutrient, nutrient_quantity=1.0, units=units
+            ),
+            domain_factories.NutritionalInformation(
+                nutrient=other_nutrient, nutrient_quantity=2.0, units=units
+            ),
+            domain_factories.NutritionalInformation(
+                nutrient=nutrient, nutrient_quantity=3.0, units=units
+            ),
+        ]
+
+        aggregated_list = ingredients.NutritionalInformation.sum_by_nutrient(
+            nutritional_information=unaggregated_list,
+            nutrients=[nutrient, other_nutrient],
+        )
+
+        assert aggregated_list == [
+            domain_factories.NutritionalInformation(
+                nutrient=nutrient, nutrient_quantity=4.0, units=units
+            ),
+            domain_factories.NutritionalInformation(
+                nutrient=other_nutrient, nutrient_quantity=2.0, units=units
+            ),
+        ]
+
+    def test_includes_zero_nutritional_information_for_unused_nutrient(
+        self,
+    ):
+        nutrient = domain_factories.Nutrient()
+        units = constants.NutrientUnit.GRAMS
+
+        aggregated_list = ingredients.NutritionalInformation.sum_by_nutrient(
+            nutritional_information=[], nutrients=[nutrient]
+        )
+
+        nutritional_information = domain_factories.NutritionalInformation(
+            nutrient=nutrient, nutrient_quantity=0.0, units=units
+        )
+        assert aggregated_list == [nutritional_information]
