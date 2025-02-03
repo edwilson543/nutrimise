ffe1c1d83b837c5bda20a0cd350328e7dfd42b4f
edwilson <edward.e.j.wilson@gmail.com>
2025-02-03 06:51:00 +0000

    Refactor recipe operation test to use new domain model factory

--- tests/integration/domain/recipes/test_operations.py
+++ tests/integration/domain/recipes/test_operations.py
@@ -4,12 +4,13 @@ from nutrimise.data.recipes import models as recipe_models
 from nutrimise.data.recipes import operations as recipe_operations
 from nutrimise.domain import embeddings
 from tests.factories import data as data_factories
+from tests.factories import domain as domain_factories
 
 
 class TestCreateOrUpdateRecipeEmbedding:
     def test_creates_new_embedding_for_recipe_with_model(self):
         recipe = data_factories.Recipe()
-        embedding = data_factories.RecipeEmbedding.build().to_domain_model()
+        embedding = domain_factories.Embedding()
         assert not recipe_models.RecipeEmbedding.objects.exists()
 
         recipe_operations.create_or_update_recipe_embedding(
