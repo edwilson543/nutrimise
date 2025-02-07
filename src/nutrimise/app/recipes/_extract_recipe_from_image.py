e3fce8b08656cdbf199d34764049bc367cd1a741
edwilson <edward.e.j.wilson@gmail.com>
2025-02-07 14:36:20 +0000

    Create missing ingredients when extracting recipe from image

--- src/nutrimise/app/recipes/_extract_recipe_from_image.py
+++ src/nutrimise/app/recipes/_extract_recipe_from_image.py
@@ -4,9 +4,10 @@ import io
 from django.contrib.auth import models as auth_models
 from PIL import Image
 
+from nutrimise.data.ingredients import operations as ingredient_operations
 from nutrimise.data.ingredients import queries as ingredient_queries
 from nutrimise.data.recipes import operations as recipe_operations
-from nutrimise.domain import image_extraction
+from nutrimise.domain import image_extraction, ingredients
 
 
 UnableToExtractRecipeFromImage = image_extraction.UnableToExtractRecipeFromImage
@@ -33,6 +34,8 @@ def extract_recipe_from_image(
         existing_ingredients=existing_ingredients,
     )
 
+    _get_or_create_ingredients(recipe=recipe)
+
     recipe_id = recipe_operations.create_recipe(
         author=author,
         name=recipe.name,
@@ -40,10 +43,6 @@ def extract_recipe_from_image(
         number_of_servings=recipe.number_of_servings,
         meal_times=recipe.meal_times,
     )
-
-    print("Discarded ingredients: ")
-    for ingredient in recipe.ingredients:
-        print(ingredient)
 
     return recipe_id
 
@@ -54,3 +53,37 @@ def _get_existing_ingredients() -> list[image_extraction.Ingredient]:
         image_extraction.Ingredient.from_domain_model(ingredient=ingredient)
         for ingredient in ingredients
     ]
+
+
+def _get_or_create_ingredients(
+    *, recipe: image_extraction.Recipe
+) -> dict[str, ingredients.Ingredient]:
+    """
+    Create any ingredients and categories extracted from the recipe that aren't in the database yet.
+
+    :return: A mapping of ingredient names to `Ingredient` domain objects.
+    """
+    ingredient_categories: dict[str, ingredients.IngredientCategory] = {}
+    ingredients_: dict[str, ingredients.Ingredient] = {}
+
+    for recipe_ingredient in recipe.ingredients:
+        category_name = recipe_ingredient.ingredient.category_name
+
+        if category_name in ingredient_categories:
+            category = ingredient_categories[category_name]
+        else:
+            category = ingredient_operations.get_or_create_ingredient_category(
+                name=category_name
+            )
+            ingredient_categories[category_name] = category
+
+        ingredient_name = recipe_ingredient.ingredient.name
+        ingredient = ingredient_operations.get_or_create_ingredient(
+            name=ingredient_name,
+            category_id=category.id,
+            units=recipe_ingredient.ingredient.units,
+            grams_per_unit=recipe_ingredient.ingredient.grams_per_unit,
+        )
+        ingredients_[ingredient_name] = ingredient
+
+    return ingredients_
--- tests/integration/app/recipes/test_extract_recipe_from_image.py
+++ tests/integration/app/recipes/test_extract_recipe_from_image.py
@@ -1,14 +1,58 @@
 import pytest
 
 from nutrimise.app.recipes import _extract_recipe_from_image
+from nutrimise.data.ingredients import models as ingredient_models
 from nutrimise.data.recipes import models as recipe_models
 from nutrimise.domain import image_extraction
 from nutrimise.domain.image_extraction import _vendors as image_extraction_vendors
 from testing.factories import data as data_factories
+from testing.factories import domain as domain_factories
 from testing.factories import images as image_factories
 
 
 def test_extracts_recipe_using_fake_extraction_service():
+    ingredient = image_extraction.Ingredient.from_domain_model(
+        domain_factories.Ingredient()
+    )
+    recipe_ingredient = image_extraction.RecipeIngredient(
+        ingredient=ingredient, quantity=37.5
+    )
+    canned_recipe = image_extraction_vendors.get_canned_recipe(
+        ingredients=[recipe_ingredient]
+    )
+
+    author = data_factories.User()
+    image = image_factories.get_image()
+    image_extraction_service = image_extraction_vendors.FakeImageExtractionService(
+        canned_recipe=canned_recipe
+    )
+
+    recipe_id = _extract_recipe_from_image.extract_recipe_from_image(
+        author=author,
+        uploaded_image=image,
+        image_extraction_service=image_extraction_service,
+    )
+
+    new_ingredient_category = ingredient_models.IngredientCategory.objects.get()
+    assert new_ingredient_category.name == ingredient.category_name
+
+    new_ingredient = ingredient_models.Ingredient.objects.get()
+    assert new_ingredient.name == ingredient.name
+    assert new_ingredient.category.id == new_ingredient_category.id
+    assert new_ingredient_category.name == ingredient.category_name
+
+    recipe = recipe_models.Recipe.objects.get()
+    assert recipe.id == recipe_id
+    assert recipe.name == image_extraction_service.canned_recipe.name
+    assert recipe.description == image_extraction_service.canned_recipe.description
+
+    # TODO.
+    # recipe_ingredient = recipe.ingredients.get()
+    # assert recipe_ingredient.ingredient_id == new_ingredient.id
+    # assert recipe_ingredient.quantity == 37.5
+
+
+def test_creates_duplicate_recipe_if_image_extraction_model_returns_existing_name():
     author = data_factories.User()
     image = image_factories.get_image()
     image_extraction_service = image_extraction_vendors.FakeImageExtractionService()
