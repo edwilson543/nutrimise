06a7aa5e9fc8d78b90ea93c8cf6f8c1e953a93d6
edwilson <edward.e.j.wilson@gmail.com>
2025-02-07 07:11:05 +0000

    Add unit tests for OpenAI image extraction service

--- src/nutrimise/domain/image_extraction/__init__.py
+++ src/nutrimise/domain/image_extraction/__init__.py
@@ -1,2 +1,8 @@
 from ._config import get_image_extraction_service
-from ._vendors import FakeImageExtractService, ImageExtractionService
+from ._constants import ImageExtractionModel, ImageExtractionVendor
+from ._vendors import (
+    FakeImageExtractService,
+    ImageExtractionService,
+    ImageExtractionServiceMisconfigured,
+    UnableToExtractRecipeFromImage,
+)
---
+++ tests/unit/domain/image_extraction/_vendors/test_openai.py
@@ -0,0 +1,92 @@
+from typing import Any
+
+import pytest
+from django.test import override_settings
+
+from nutrimise.domain import image_extraction
+from nutrimise.domain.image_extraction._vendors import _openai
+
+
+class TestInstantiation:
+    def tests_instantiates_service_when_api_key_set(self):
+        with override_settings(OPENAI_API_KEY="some-key"):
+            _openai.OpenAIImageExtractService()
+
+    @pytest.mark.parametrize("api_key", ["", None])
+    def test_raises_configuration_error_when_api_key_not_set(self, api_key: str | None):
+        with (
+            override_settings(OPENAI_API_KEY=api_key),
+            pytest.raises(image_extraction.ImageExtractionServiceMisconfigured) as exc,
+        ):
+            _openai.OpenAIImageExtractService()
+
+        assert exc.value.vendor == image_extraction.ImageExtractionVendor.OPENAI
+
+
+class TestExtractRecipeFromImage:
+    @override_settings(OPENAI_API_KEY="some-key")
+    def test_gets_output_structured_as_recipe(self, httpx_mock):
+        openai_service = _openai.OpenAIImageExtractService()
+        base64_image = "My encoded image."
+
+        httpx_mock.add_response(
+            url="https://api.openai.com/v1/chat/completions",
+            method="POST",
+            status_code=200,
+            json=self._response_ok_json(),
+        )
+
+        recipe = openai_service.extract_recipe_from_image(base64_image=base64_image)
+
+        assert recipe.name == "Some recipe"
+        assert recipe.description == "Some recipe description."
+
+    @override_settings(OPENAI_API_KEY="some-key")
+    def test_raises_when_open_ai_api_response_bad(self, httpx_mock):
+        openai_service = _openai.OpenAIImageExtractService()
+        base64_image = "My encoded image."
+
+        httpx_mock.add_response(
+            url="https://api.openai.com/v1/chat/completions",
+            method="POST",
+            status_code=401,
+        )
+
+        with pytest.raises(image_extraction.UnableToExtractRecipeFromImage) as exc:
+            openai_service.extract_recipe_from_image(base64_image=base64_image)
+
+        assert exc.value.vendor == openai_service.vendor
+        assert exc.value.model == openai_service.model
+
+    def _response_ok_json(self) -> dict[str, Any]:
+        """
+        OpenAI chat completion structure response output, per the docs.
+        https://platform.openai.com/docs/guides/structured-outputs
+        """
+        return {
+            "id": "chatcmpl-AyCk92plBjBPBOnrcpYQ4xVC7LPZd",
+            "choices": [
+                {
+                    "finish_reason": "stop",
+                    "index": 0,
+                    "logprobs": None,
+                    "message": {
+                        "content": '{"name":"Some recipe","description":"Some recipe description."}',
+                        "refusal": None,
+                        "role": "assistant",
+                        "audio": None,
+                        "function_call": None,
+                        "tool_calls": [],
+                        "parsed": {
+                            "name": "Some recipe",
+                            "description": "Some recipe description.",
+                        },
+                    },
+                }
+            ],
+            "created": 1738912029,
+            "model": "gpt-4o-2024-08-06",
+            "object": "chat.completion",
+            "service_tier": "default",
+            "system_fingerprint": "fp_4691090a87",
+        }
