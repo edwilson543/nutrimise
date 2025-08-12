73ccd242184bbd880f83b6c52618e10626be038b
edwilson <edward.e.j.wilson@gmail.com>
2025-08-12 14:23:13 +0100

    Setup asgi for fronend api

--- backend/src/nutrimise/config/asgi.py
+++ backend/src/nutrimise/config/asgi.py
@@ -5,6 +5,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nutrimise.config.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Settings")
 
 if __name__ == "__main__":
+    from configurations.asgi import get_asgi_application
+
+    application = get_asgi_application()
+
     import uvicorn
 
     from nutrimise.interfaces.frontend_api import app
