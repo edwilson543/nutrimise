6a8698e3934c8189feb81afe41ddac4419f79a8d
edwilson <edward.e.j.wilson@gmail.com>
2025-08-13 11:08:53 +0100

    Add `SavedRecipe` model

--- backend/src/nutrimise/data/recipes/models/__init__.py
+++ backend/src/nutrimise/data/recipes/models/__init__.py
@@ -2,3 +2,4 @@ from ._recipe import Recipe
 from ._recipe_author import RecipeAuthor
 from ._recipe_embedding import RecipeEmbedding
 from ._recipe_ingredient import RecipeIngredient
+from ._saved_recipe import SavedRecipe
