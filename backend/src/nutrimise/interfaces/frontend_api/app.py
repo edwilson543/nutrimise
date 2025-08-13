802ccb30bca14f493b938f283888a8488714b0a9
edwilson <edward.e.j.wilson@gmail.com>
2025-08-13 09:48:15 +0100

    Allow access to API from localhost 8080

--- backend/src/nutrimise/interfaces/frontend_api/app.py
+++ backend/src/nutrimise/interfaces/frontend_api/app.py
@@ -1,9 +1,18 @@
 import fastapi
+from fastapi.middleware import cors
 
 from . import routers
 
 
 api = fastapi.FastAPI()
+
+api.add_middleware(
+    cors.CORSMiddleware,
+    allow_origins=["http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 api.include_router(routers.general_router, prefix="/general")
 api.include_router(routers.recipe_router, prefix="/recipes")
